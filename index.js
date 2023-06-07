58cb1b4740b21ddcfc8978a7a3c7b5d3fe7d865e
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-06-07 18:54:45 +0600

    Server Runs Successfully

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req, res) =>{
+    res.send('School is Open')
+})
+
+app.listen(port, () =>{
+    console.log(`Camp School is Sitting on port ${port}`);
+})
