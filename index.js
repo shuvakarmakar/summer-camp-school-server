7d4a7655cd631f7c6a3dda9b423f9155a530b170
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-06-09 13:11:29 +0600

    Admin Role Select Route Setup Done

--- index.js
+++ index.js
@@ -50,6 +50,18 @@ async function run() {
             res.send(result);
         });
 
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: "admin"
+                },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
         // instructor
         app.get('/instructors', async (req, res) => {
             const result = await instructorCollection.find().toArray();
